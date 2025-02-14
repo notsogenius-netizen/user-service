1c5f39d52689620beb225b109190867605e9bef5
notsogenius-netizen <sourabhchangmai11@gmail.com>
2025-02-14 10:51:08 +0530

    add user and history entities with JPA annotations

---
+++ src/main/java/com/sourabh/user_service/entities/History.java
@@ -0,0 +1,29 @@
+package com.sourabh.user_service.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "history")
+public class History {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String origin;
+    private String destination;
+    private Date travelDateTime;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ src/main/java/com/sourabh/user_service/entities/User.java
@@ -0,0 +1,27 @@
+package com.sourabh.user_service.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<History> travelHistory;
+}
